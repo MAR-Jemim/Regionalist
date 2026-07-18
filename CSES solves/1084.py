eb9827aab5c01dd5fb02343a96417bceffd69ca8
proBOT <marjemim966@gmail.com>
2026-07-18 09:35:43 +0600

    .

--- CSES solves/1084.py
+++ CSES solves/1084.py
@@ -1,12 +1,5 @@
-"""This repo is to store my journey of CP.
-I will store my solves of cf problems"""
-
-# template
-
-"""
-
-# Problem: 
-# Limits:  MB | 1.00 s
+# Problem: Apartments | https://cses.fi/problemset/task/1084/
+# Limits: 512 MB | 1.00 s
 # Bismillahir Rahmanir Rahim
 
 import sys
@@ -18,5 +11,3 @@ sys.stderr = open("../output.txt", 'a')
 # Delete
 
 print('hi')
-
-"""
